c763d16b950023a5d0ed9610b329b4a9afa53eba
GOTTIPATI GOPI SAI <gottipatigopisai@gmail.com>
2025-01-31 23:10:58 +0530

    first commit

---
+++ src/main/java/com/jiraslack/jira_slack_backend/JiraSlackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jiraslack.jira_slack_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraSlackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiraSlackBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jiraslack/jira_slack_backend/JiraSlackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiraslack.jira_slack_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiraSlackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
